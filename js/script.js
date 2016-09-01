0bd75e57cb6a6b7aa2a8ba72ae51e041ed8f7001
Danil Zaviryukha <logolevel@gmail.com>
2016-09-01 18:06:54 +0300

    Added tooltips

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	/* tabs */
     $(".tabs__link").click(function(event) {
         event.preventDefault();
         $(this).parent().addClass("tabs__current");
@@ -7,4 +8,25 @@ $(document).ready(function() {
         $(".tabs__content").not(tab).css("display", "none");
         $(tab).fadeIn();
     });
+
+/* tooltips */
+//function show tooltip
+	var showTool = function showTooltip() {
+		name = $(this).attr('name');
+		$('label.fieldHint[for="'+name+'"]').fadeIn('slow');
+	};
+//function hide tooltip
+	var hideTool = function hideTooltip() {
+		var name = $(this).attr('name');
+		$('label.fieldHint[for="'+name+'"]').fadeOut('slow');
+	};
+
+//focus
+		$('#myForm input').focus(showTool)
+		.blur(hideTool);
+
+//hover
+		$('#myForm input').hover(showTool, hideTool);
+		
 });
+
